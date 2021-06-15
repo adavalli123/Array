f5bc8f7f154444f86d88f70cf923c6cb055fc996
srikanth_iOS <adavalli123@gmail.com>
2021-06-14 19:18:01 -0500

    Update Rotation.swift

--- Rotation.swift
+++ Rotation.swift
@@ -1,1 +1,13 @@
 
+// by creating a new array
+func rotate(_ nums: inout [Int], _ k: Int) {
+  var temp = nums
+  
+  for i in 0 ..< nums.count {
+    temp[(i + k) % nums.count] = nums[i]
+  }
+  
+  nums = temp
+}
+
+assert(rotate([1,2,3,4,5,6,7], 3)) // [5,6,7,1,2,3,4]
