6b64bf899106140b1fed887932c50f68a567b246
srikanth_iOS <adavalli123@gmail.com>
2021-06-14 19:50:50 -0500

    Update Rotation.swift

--- Rotation.swift
+++ Rotation.swift
@@ -1,13 +1,33 @@
 
-// by creating a new array
-func rotate(_ nums: inout [Int], _ k: Int) {
-  var temp = nums
+
+class Solution {
+// by creating a new array  
+  func rotate(_ nums: inout [Int], _ k: Int) {
+    var temp = nums
   
-  for i in 0 ..< nums.count {
-    temp[(i + k) % nums.count] = nums[i]
+    for i in 0 ..< nums.count {
+      temp[(i + k) % nums.count] = nums[i]
+    }
+  
+    nums = temp
   }
   
-  nums = temp
+    func rotate(_ nums: inout [Int], _ k: Int) {
+        var k = k % nums.count
+        reverse(&nums, 0, nums.count-1)
+        reverse(&nums, 0, k-1)
+        reverse(&nums, k, nums.count-1)
+    }
+    
+    func reverse(_ nums: inout [Int], _ s: Int, _ e: Int) {
+        var s = s 
+        var e = e 
+        while s < e {
+            nums.swapAt(s, e)
+            s += 1
+            e -= 1
+        }
+    }
 }
 
 assert(rotate([1,2,3,4,5,6,7], 3)) // [5,6,7,1,2,3,4]
